752308bf1fd592d3857f87912af41ec8abdd1c0b
Joel <1105039@est.intec.edu.do>
2023-06-06 14:47:18 -0400

    added logo

--- hive/src/app/components/general/Card.js
+++ hive/src/app/components/general/Card.js
@@ -3,14 +3,20 @@ import Image from 'next/image'
 
 export default function Card({ User, Message }) {
     return (
-        <div className='flex min-w-[100%] min-h-[150px] bg-slate-500 rounded-xl p-2 shadow-md'>
-            <div className='min-w-[10%] items-center mx-auto'>
-                <Image className='w-[5rem] h-[5rem] bg-red-200 rounded-full self-center' />
+        <div className='flex flex-col min-w-[100%] min-h-[100px] bg-slate-500 rounded-xl p-2 shadow-md'>
+            < div className='flex '>
+                < div className='min-w-[10%] items-center mx-auto' >
+                    <Image className='w-[50px] h-[50px] bg-red-200 rounded-full self-center' />
+                </div >
+                <div className='flex flex-col min-w-[90%]'>
+                    <span className='font-medium text-3xl'>{User}</span>
+                    <p>{Message}</p>
+                </div>
+                {/* buttons for interaction */}
+            </div >
+            <div className='flex w-[100%] items-baseline'>
+                <p>hola</p>
             </div>
-            <div className='flex flex-col min-w-[90%]'>
-                <span className='font-medium text-3xl'>{User}</span>
-                <p>{Message}</p>
-            </div>
-        </div>
+        </div >
     );
 }
--- hive/src/app/page.js
+++ hive/src/app/page.js
@@ -1,17 +1,30 @@
 import Card from '@/app/components/general/Card';
+import Image from 'next/image';
+import logo from '@/app/logo.svg'
 import { dPadding } from '@/app/constants';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-[60%] flex-col items-center justify-between justify-self-center mx-auto bg-red">
+    <main className="flex min-h-screen w-[75%] flex-col items-center justify-between justify-self-center mx-auto bg-red">
       <div className='w-full h-screen flex'>
-        <div className='min-h-screen w-[20%] bg-slate-50 flex flex-col px-10 py-5'>
-
+        <div className='min-h-screen w-[20%] flex flex-col px-5 py-5 border-x-2 border-slate-50'>
+          {/* Seccion dle logo */}
+          <div className='flex w-full gap-2 items-center'>
+            <Image src={logo} className='w-[6rem] h-[5rem]'></Image>
+            <span className='text-[2.5rem]'>HIVE</span>
+          </div>
+          {/* Menu */}
+          <div className='flex flex-col'>
+            {/* botones */}
+            <div></div>
+          </div>
         </div>
-        <div className='min-h-screen w-[60%] bg-slate-200 flex flex-col px-5 py-5'>
+        <div className='min-h-screen w-[60%]  flex flex-col px-5 py-5 gap-2'>
+          <h1>What's new?</h1>
+
           <Card User={'Laura'} Message={'Saludos'} />
         </div>
-        <div className='min-h-screen w-[20%] bg-slate-50 flex flex-col'>
+        <div className='min-h-screen w-[20%] flex flex-col border-x-2 border-slate-50'>
 
         </div>
 

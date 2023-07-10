b6292619c5d6fd158924e50c95fc8de1b6bad1c1
JoelMauricioHdez <joel.mauricio@hecmapp.com>
2023-07-10 17:19:49 -0400

    added placeholder for follow and following

--- hive/src/app/components/general/NoResultsComponent.js
+++ hive/src/app/components/general/NoResultsComponent.js
@@ -3,7 +3,7 @@ import IconNoPosts from "@/app/icons/NotResults"
 import IconBookmark from "@/app/icons/save"
 import { useAuthContext } from "@/app/context/authentication"
 
-export default function NotResultsComp({ favorites = false, userId }) {
+export default function NotResultsComp({ favorites = false, userId, CustomText }) {
     const { profile } = useAuthContext()
     const iconFormat = "w-[4.5rem] h-[4.5rem]"
 
@@ -12,7 +12,7 @@ export default function NotResultsComp({ favorites = false, userId }) {
             favorites ? <IconBookmark className={iconFormat} /> :
                 <IconNoPosts className={iconFormat} />
         }
-        <span>There are no {favorites == true ? "favorites posts" : "posts"}</span>
+        <span>{CustomText != undefined ? CustomText : favorites == true ? "There are no favorites posts" : "There are no posts"}</span>
         {userId === profile ? <span className="text-xl font-semibold">{favorites == true ? "GO and ADD your favorite posts!" : "GO and POST something!"}</span> : null}
     </div>
 }
